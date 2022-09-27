8700944e7044a279e18564e03db9c44e696ef00d
Claireehallock <91226619+Claireehallock@users.noreply.github.com>
2022-09-27 10:19:18 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+	//command line related - done by Abdurrahman Kazi
+  public static void main(String[] args) {
+		
+    if (args.length != 3) {
+		 	System.out.println("Usage: java Main.java sourceFile targetFile");
+		 	System.exit(1);
+		 }
+    
+
+		String sourceFileName = args[0];//source.txt
+    String compressedFileName = args[1];//compressedFile.txt
+    String targetFileName = args[2];//target.txt
+
+        Compress compressor = new Compress();
+		compressor.compressFile(sourceFileName, compressedFileName); 
+
+        Decompress decompressor = new Decompress();
+        decompressor.decompressFile(compressedFileName, targetFileName);
+
+        System.out.println("Done!");
+	}
+}
